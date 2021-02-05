03ae0954d75f046d974e57a9ee915009d4f52859
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2021-02-05 11:40:12 +0000

    Update shiny
    cleaned up legacy debugging and comments
    tidied up code style

--- shiny/amb_checker_all_web/global.R
+++ shiny/amb_checker_all_web/global.R
@@ -21,9 +21,11 @@ readambweblive <- function(x) {
 
 ## So we don't keep hammering NHSE's website this picks up the data from the rda
 readambweb <- function(x) {
-if(x == "ambsys") {ambsys
-      } else if(x == "ambco") {ambco
-    } else {
+  if (x == "ambsys") {
+    ambsys
+  } else if (x == "ambco") {
+    ambco
+  } else {
   }
 }
 
@@ -42,7 +44,7 @@ processdata <- function(extract) {
   ## Determine the periods required
   periods <- paste(amb$Year, amb$Month, sep = "-") %>% unique()
   periods1 <- periods
-  
+
   ## Create data frame to hold test outputs
   output <- data.frame(
     Period = character(),
@@ -62,29 +64,29 @@ processdata <- function(extract) {
     test7false = numeric(),
     dashes = numeric()
   )
-  
+
   ## Loop through whole process for each period
   for (period in (1:(length(periods1)))) {
     yearmonth <- periods1[period]
     output[period, ] <- c(yearmonth, rep(0, each = 15))
-    
+
     amb_filtered <- amb %>% dplyr::filter(paste(Year, Month, sep = "-") == yearmonth)
-    
+
     ## Check for dashes - if this is not zero needs to be fixed in the file
     dashes <- grep("–|-", amb_filtered)
-    
+
     ## Convert strings to numeric and cleanse (first 5 columns not to be changed)
     amb_filtered[, -c(1:5)] <- suppressWarnings(apply(amb_filtered[, -c(1:5)], 2, as.numeric))
     amb_filtered[is.na(amb_filtered)] <- 0
-    
+
     ## Identify region and all england rows
     regions <- grep("Y[0-9][0-9]", amb_filtered$Org.Code)
     england <- which(amb_filtered$Org.Code == "Eng")
-    
+
     ## Identify columns which are to be weighted summed and meaned
     ## - There is no pattern for ambsys so has to be hard coded
     ## - Ambco we can use some logic to identify the weighted columns, then everything else is summed
-    
+
     ## weighted
     if (extract == "ambco") {
       weighted <- union(
@@ -112,22 +114,22 @@ processdata <- function(extract) {
     } else {
       print("Unknown Parameter: Extract")
     }
-    
+
     ## summed
     if (extract == "ambco") {
       summed <- which(names(amb_filtered) %in% names(amb_filtered)[-weighted][-c(1:5)])
     } else if (extract == "ambsys") {
       summed <- which(names(amb_filtered) %in%
-                        c(
-                          "A0", "A1", "A2", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A18", "A19", "A20", "A21", "A22", "A23", "A24", "A27", "A30",
-                          "A33", "A36", "A49", "A50", "A53", "A56", "A57", "A58", "A59", "A60", "A61", "A62", "A65", "A68", "A71", "A74", "A75", "A76", "A77", "A78",
-                          "A79", "A80", "A81", "A82", "A85", "A88", "A91", "A94", "A97", "A100", "A103", "A106", "A107", "A110", "A111", "A112", "A113", "A115", "A116",
-                          "A119", "A120", "A17", "A39", "A40", "A41", "A42", "A43", "A44", "A45", "A46", "A47", "A48", "A54", "A55"
-                        ))
+        c(
+          "A0", "A1", "A2", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A18", "A19", "A20", "A21", "A22", "A23", "A24", "A27", "A30",
+          "A33", "A36", "A49", "A50", "A53", "A56", "A57", "A58", "A59", "A60", "A61", "A62", "A65", "A68", "A71", "A74", "A75", "A76", "A77", "A78",
+          "A79", "A80", "A81", "A82", "A85", "A88", "A91", "A94", "A97", "A100", "A103", "A106", "A107", "A110", "A111", "A112", "A113", "A115", "A116",
+          "A119", "A120", "A17", "A39", "A40", "A41", "A42", "A43", "A44", "A45", "A46", "A47", "A48", "A54", "A55"
+        ))
     } else {
       print("Unknown Parameter: Extract")
     }
-    
+
     ## mean - this only applies to ambsys
     meanlookup <- data.frame(
       metric =
@@ -147,8 +149,8 @@ processdata <- function(extract) {
         )
     )
     meaned <- which(names(amb_filtered) %in% meanlookup$metric)
-    
-    
+
+
     ## Map trusts to regions
     RegionMap <- cbind(
       amb_filtered[-regions, c("Org.Code", "Org.Name")],
@@ -156,15 +158,15 @@ processdata <- function(extract) {
         dplyr::filter(!is.na(Org.Code))
     )
     names(RegionMap)[c(3:4)] <- c("Region.Code", "Region.Name")
-    
+
     ## Regions with more than 1 trust
     Regions.gt.1trust <- data.frame(sort(table(RegionMap$Region.Name), decreasing = T)) %>% dplyr::filter(Freq > 1)
     rc <- unique(RegionMap$Region.Code[which(RegionMap$Region.Name %in% Regions.gt.1trust$Var1)])
-    
+
     ## Single regions
     RegionMapSingle <- RegionMap[-which(RegionMap$Region.Code %in% rc), ][-england, ]
-    
-    
+
+
     ## Run calculations to compare trust data to england
     ## Summed Columns
     amb_filtered_summed <- cbind(
@@ -172,7 +174,7 @@ processdata <- function(extract) {
       t(amb_filtered[england, c(summed)])
     )
     names(amb_filtered_summed)[2] <- "England"
-    
+
     ## meaned columns
     if (extract == "ambsys") {
       amb_filtered_meaned <- sapply(c(1:length(meaned)), function(x) {
@@ -194,15 +196,15 @@ processdata <- function(extract) {
       names(amb_filtered_meaned) <- c("total", "England")
     } else { # do nothing
     }
-    
+
     ## weighted columns
     ## Do the weighted average by sum product for each trust
     weightedt <- sapply(c(1:length(weighted)), function(x) {
       sum(amb_filtered[-regions, weighted][-england, ][, x] *
-            amb_filtered[-regions, weightedlookup$denom[match(
-              names(amb_filtered[-regions, weighted][-england, ]),
-              weightedlookup$num
-            )]][-england, ][, x])
+        amb_filtered[-regions, weightedlookup$denom[match(
+          names(amb_filtered[-regions, weighted][-england, ]),
+          weightedlookup$num
+        )]][-england, ][, x])
     })
     weightede <- as.numeric(amb_filtered[england, weightedlookup$denom[match(names(amb_filtered[england, weighted]), weightedlookup$num)]])
     ## divide by the england figure and combine calculated and value in download into data frame
@@ -214,21 +216,21 @@ processdata <- function(extract) {
     names(amb_filtered_weighted)[2] <- "England"
     ## old periods don't have the weighted metrics - set to 0 if they error out
     amb_filtered_weighted[is.na(amb_filtered_weighted)] <- 0
-    
+
     ## Loop through each region with more than 1 trust for summed columns then repeat for the weighted ones
     mylist <- vector("list", length = length(rc)) # summed
     mylistw <- vector("list", length = length(rc)) # weighted
     mylistm <- vector("list", length = length(rc)) # meaned
-    
+
     i <- 2
     for (i in (1:length(rc))) {
       j <- rc[i]
       k <- which(amb_filtered$Org.Code %in% RegionMap$Org.Code[which(RegionMap$Region.Code == rc[i])])
       m <- which(amb_filtered$Org.Code == j)
-      
+
       ## summed columns
       mylist[[i]] <- cbind(data.frame(total = apply(amb_filtered[k, summed], 2, sum)), region = t(amb_filtered[m, summed]))
-      
+
       ## meaned columns - only for ambsys
       if (extract == "ambsys") {
         mylistm[[i]] <- sapply(c(1:length(meaned)), function(x) {
@@ -249,7 +251,7 @@ processdata <- function(extract) {
           cbind(., t(amb_filtered[m, meaned]))
       } else { # do nothing
       }
-      
+
       ## weighted columns
       weightedew <- sapply(c(1:length(weighted)), function(x) {
         sum(amb_filtered[k, weighted][, x] * amb_filtered[k, weightedlookup$denom[match(
@@ -264,16 +266,16 @@ processdata <- function(extract) {
         cbind(weightedew / weightedtw, t(amb_filtered[m, weighted]))
       }
     }
-    
+
     # rm(i,j,k,m)
-    
+
     ## add names to each list for all levels
     names(mylist) <- rc
     mylist <- lapply(seq_along(mylist), function(i) {
       colnames(mylist[[i]]) <- c(names(mylist)[i], "Region")
       mylist[[i]]
     })
-    
+
     if (extract == "ambsys") {
       names(mylistm) <- rc
       mylistm <- lapply(seq_along(mylistm), function(i) {
@@ -282,20 +284,20 @@ processdata <- function(extract) {
       })
     } else { # do nothing
     }
-    
+
     names(mylistw) <- rc
     mylistw <- lapply(seq_along(mylistw), function(i) {
       colnames(mylistw[[i]]) <- c(names(mylistw)[i], "Region")
       mylistw[[i]]
     })
-    
+
     ## patch NaN with 0
     mylistw <- rapply(mylistw, f = function(x) ifelse(is.nan(x), 0, x), how = "replace")
-    
+
     ####################################
     ## Tests
     ####################################
-    
+
     ## test summed columns = england
     output[period, 2] <-
       data.frame(table(amb_filtered_summed$total == amb_filtered_summed$England)) %>%
@@ -303,14 +305,14 @@ processdata <- function(extract) {
       select(Freq)
     output[period, 3] <-
       if (nrow(data.frame(table(amb_filtered_summed$total == amb_filtered_summed$England)) %>%
-               filter(Var1 == F)) > 0) {
+        filter(Var1 == F)) > 0) {
         data.frame(table(amb_filtered_summed$total == amb_filtered_summed$England)) %>%
           filter(Var1 == F) %>%
           select(Freq)
       } else {
         0
       }
-    
+
     ## test meaned columns = england
     output[period, 4] <-
       if (extract == "ambsys") {
@@ -320,11 +322,11 @@ processdata <- function(extract) {
       } else {
         0
       }
-    
+
     output[period, 5] <-
       if (extract == "ambsys") {
         if (nrow(data.frame(table(amb_filtered_meaned[, 1] == amb_filtered_meaned[, 2])) %>%
-                 filter(Var1 == F)) > 0) {
+          filter(Var1 == F)) > 0) {
           data.frame(table(amb_filtered_meaned[, 1] == amb_filtered_meaned[, 2])) %>%
             filter(Var1 == F) %>%
             select(Freq)
@@ -334,8 +336,8 @@ processdata <- function(extract) {
       } else {
         0
       }
-    
-    
+
+
     ## test weighted columns = england rounded to 0 dps for ambsys use all.equal function for ambco
     if (extract == "ambsys") {
       output[period, 6] <-
@@ -351,11 +353,11 @@ processdata <- function(extract) {
         filter(Var1 == T) %>%
         select(Freq)
     }
-    
+
     if (extract == "ambsys") {
       output[period, 7] <-
         if (nrow(data.frame(table(rnd(amb_filtered_weighted$total) == rnd(amb_filtered_weighted$England))) %>%
-                 filter(Var1 == F)) > 0
+          filter(Var1 == F)) > 0
         ) {
           data.frame(table(rnd(amb_filtered_weighted$total) == rnd(amb_filtered_weighted$England))) %>%
             filter(Var1 == F) %>%
@@ -369,7 +371,7 @@ processdata <- function(extract) {
         if (nrow(data.frame(table(mapply(function(x, y) {
           isTRUE(all.equal(x, y))
         }, amb_filtered_weighted$total, amb_filtered_weighted$England))) %>%
-        filter(Var1 == F)) > 0) {
+          filter(Var1 == F)) > 0) {
           # data.frame(table(format(amb_filtered_weighted$total, scientific = T, digits = 7) == format(amb_filtered_weighted$England, scientific = T, digits = 7))) %>%
           data.frame(table(mapply(function(x, y) {
             isTRUE(all.equal(x, y))
@@ -380,7 +382,7 @@ processdata <- function(extract) {
           0
         }
     }
-    
+
     ## test summed columns match region
     testregions <- data.frame(lapply(c(1:length(mylist)), function(i) mylist[[i]][, 1] == mylist[[i]][, 2]))
     names(testregions) <- rc # c("Region1", "Region2", "Region3")
@@ -388,17 +390,17 @@ processdata <- function(extract) {
       data.frame(table(rbind(testregions[, 1], testregions[, 2], testregions[, 3]))) %>%
       filter(Var1 == T) %>%
       select(Freq)
-    
+
     output[period, 9] <-
       if (nrow(data.frame(table(rbind(testregions[, 1], testregions[, 2], testregions[, 3]))) %>%
-               filter(Var1 == F)) > 0) {
+        filter(Var1 == F)) > 0) {
         data.frame(table(rbind(testregions[, 1], testregions[, 2], testregions[, 3]))) %>%
           filter(Var1 == F) %>%
           select(Freq)
       } else {
         0
       }
-    
+
     ## test meaned columns match region
     if (extract == "ambsys") {
       testregionsm <- data.frame(lapply(c(1:length(mylistm)), function(i) mylistm[[i]][, 1] == mylistm[[i]][, 2]))
@@ -409,7 +411,7 @@ processdata <- function(extract) {
         select(Freq)
       output[period, 11] <-
         if (nrow(data.frame(table(rbind(testregionsm[, 1], testregionsm[, 2], testregionsm[, 3]))) %>%
-                 filter(Var1 == F)) > 0) {
+          filter(Var1 == F)) > 0) {
           data.frame(table(rbind(testregionsm[, 1], testregionsm[, 2], testregionsm[, 3]))) %>%
             filter(Var1 == F) %>%
             select(Freq)
@@ -419,15 +421,8 @@ processdata <- function(extract) {
     } else {
       0
     }
-    
+
     ## test weighted columns match region
-    
-    # testregionsw <- data.frame(lapply(
-    #  c(1:length(mylistw)),
-    #  function(i)
-    #    format(mylistw[[i]][, 1], scientific = T, digits = 7) == format(mylistw[[i]][, 2], scientific = T, digits = 7)
-    # ))
-    
     testregionsw <- data.frame(lapply(
       c(1:length(mylistw)),
       function(i) {
@@ -436,23 +431,23 @@ processdata <- function(extract) {
         }, mylistw[[i]][, 1], mylistw[[i]][, 2])
       }
     ))
-    
+
     names(testregionsw) <- rc # c("Region1", "Region2", "Region3")
     output[period, 12] <-
       data.frame(table(rbind(testregionsw[, 1], testregionsw[, 2], testregionsw[, 3]))) %>%
       filter(Var1 == T) %>%
       select(Freq)
-    
+
     output[period, 13] <-
       if (nrow(data.frame(table(rbind(testregionsw[, 1], testregionsw[, 2], testregionsw[, 3]))) %>%
-               filter(Var1 == F)) > 0) {
+        filter(Var1 == F)) > 0) {
         data.frame(table(rbind(testregionsw[, 1], testregionsw[, 2], testregionsw[, 3]))) %>%
           filter(Var1 == F) %>%
           select(Freq)
       } else {
         0
       }
-    
+
     ## test single trust to single region
     RegionMapSingle <- RegionMap[-which(RegionMap$Region.Code %in% rc), ][-england, ]
     SingleRegion <- amb_filtered[which(amb_filtered$Org.Code %in% RegionMapSingle$Org.Code), -c(1:4)] %>%
@@ -460,31 +455,26 @@ processdata <- function(extract) {
       select(-1) == amb_filtered[which(amb_filtered$Org.Code %in% RegionMapSingle$Region.Code), -c(1:4)] %>%
       arrange(Org.Name) %>%
       select(-1)
-    
+
     output[period, 14] <- data.frame(`TRUE` = length(which(SingleRegion == T)))
     output[period, 15] <- data.frame(`FALSE` = length(which(SingleRegion == F)))
   }
-  
-  #period <- periods1[1]
-  output[period, 16] <- if(length(dashes) == 0) 0 else length(dashes)
-  
+
+  output[period, 16] <- if (length(dashes) == 0) 0 else length(dashes)
+
   ## reformat outpput and check false columns total
   fc <- grep("false", names(output))
   output[, -1] <- suppressWarnings(apply(output[, -1], 2, as.integer))
-  
+
   output$Period <- as.Date(paste(output$Period,
-                                 NumberOfDays(as.Date(paste(output$Period, "01", sep = "-"), "%Y-%m-%d")),
-                                 sep = "-"
+    NumberOfDays(as.Date(paste(output$Period, "01", sep = "-"), "%Y-%m-%d")),
+    sep = "-"
   ), "%Y-%m-%d") %>% as.character()
 
-    output <- output %>%
+  output <- output %>%
     arrange(desc(Period)) %>%
     data.frame()
 
-  #output <- head(amb,10)  
   ## Return output to the go into output$datasetInput
   return(output)
 }
-
-
-
