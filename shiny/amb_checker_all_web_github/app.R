47fcb445439a23b3fa2e51fa9f9b44dc6e39a5b3
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2022-10-25 14:24:05 +0100

    Update app.R

--- shiny/amb_checker_all_web_github/app.R
+++ shiny/amb_checker_all_web_github/app.R
@@ -1,4 +1,4 @@
 ## Wrapper for app.R on github
 ## This is what will get published and be run
-source('https://raw.githubusercontent.com/LPulle/NHSE-Analytics/master/shiny/amb_checker_all_web_github/app.R')
+source('https://raw.githubusercontent.com/LPulle/NHSE-Analytics/master/shiny/amb_checker_all_web_github/app_source.R')
 shinyApp(ui, server)
