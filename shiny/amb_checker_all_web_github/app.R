c9e230900014ff15c916ab18e53af4a6a89eade7
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2022-10-25 14:23:29 +0100

    Create app.R

---
+++ shiny/amb_checker_all_web_github/app.R
@@ -0,0 +1,4 @@
+## Wrapper for app.R on github
+## This is what will get published and be run
+source('https://raw.githubusercontent.com/LPulle/NHSE-Analytics/master/shiny/amb_checker_all_web_github/app.R')
+shinyApp(ui, server)
